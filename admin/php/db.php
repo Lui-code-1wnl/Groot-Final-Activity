9401245873b8e77cbdf669d07fa3b6bca02a1009
JOSE MIGUEL DE JESUS <2221095@slu.edu.ph>
2023-11-22 21:56:59 +0800

    update db.php in terms of connecting to db

--- admin/php/db.php
+++ admin/php/db.php
@@ -1,4 +1,15 @@
 <?php
+$host = 'localhost'; // (jdbc:mysql://localhost:3306/wordy_db) not sure if need all of this
+$dbname = 'groot_final';
+$username = 'root';
+$password = '';
 
-    $db = new mysqli("p:localhost", "root", "", "");
+// Create a connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
