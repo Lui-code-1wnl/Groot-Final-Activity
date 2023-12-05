e2deb3eb64d93f928105928aa8c9a56f81dfac12
tiaamoreyytt <2222848@slu.edu.ph>
2023-12-05 20:29:31 +0800

    Welcome page

--- assets/script/app.js
+++ assets/script/app.js
@@ -27,6 +27,15 @@ app.use(session({secret: 'somesecretkey', resave: true, saveUninitialized: true}
 app.listen(portNumber, hostIP)
 console.log(`Server running on port number ${portNumber}`);
 
+app.get('/', (request, response) => {
+        response.render('index');
+});
+
+app.get('/login', (request, response) => {
+    var username = request.session.username;
+    response.render('login', { username: username });
+});
+
 app.post('/login', (request, response) => {
     var username = request.body.username;
     var password = request.body.password;
@@ -35,23 +44,38 @@ app.post('/login', (request, response) => {
         connection.query('SELECT * FROM user WHERE username = ? and password = ?',
             [username, password],
             function(err, result, fields) {
-                if (err) throw err;
-
-                if (result.length > 0) {
-                    request.session.username = username;
-                    request.session.shcart = [];
-                    response.redirect('/');
-                    console.log('Logged in successfully.');
+                if (err) {
+                    console.error(err);
+                    response.redirect('/login'); // Redirect on error
                 } else {
-                    console.log("Login failed.");
-                    response.redirect('/');
+                    if (result.length > 0) {
+                        const user = result[0];
+                        const role = user.userRole;
+
+                        if (role === 'user') {
+                            console.log(`${username} is a ${role}`);
+                        } else if (role === 'office') {
+                            console.log(`${username} is a ${role}`);
+                        }
+
+                        request.session.username = username;
+                        request.session.role = role;
+                        request.session.shcart = [];
+                        response.redirect('/'); // Redirect after successful login
+                        console.log('Logged in successfully.');
+                    } else {
+                        console.log("Login failed.");
+                        response.redirect('/login'); // Redirect if login fails
+                    }
                 }
             }
         );
+    } else {
+        response.redirect('/login'); // Redirect if username/password not provided
     }
 });
 
 app.get('/', (request, response) => {
-    var username =request.session.usernamel
+    var username =request.session.username
     response.render('login',{username:username})
 });
