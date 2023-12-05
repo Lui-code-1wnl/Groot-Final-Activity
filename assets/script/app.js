1137f5527a5f5ff30ef2a6c3f89f6fc1b976f313
tiaamoreyytt <2222848@slu.edu.ph>
2023-12-05 14:15:23 +0800

    Working log in and connecting to node

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,19 +2,56 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const portNumber = 6969;
-
+const {request, response} = require("express");
+const portNumber = 10000;
 const hostIP = 'localhost'
-
+var path = require('path')
 var connection = mysql.createConnection({
-    host: hostIP, user:'root', password: '', database: 'groot_finals'
+    host: hostIP, user:'root', password: '', database: 'groot_final'
 });
 
 const app = express();
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded);
+app.set('views', `${__dirname}/public/view`);
+app.set('view engine', 'pug');
+app.use('/css', express.static(`${__dirname}/script/public/css`));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 app.use(session({secret: 'somesecretkey', resave: true, saveUninitialized: true}));
 
+
+
 app.listen(portNumber, hostIP)
-console.log('Server running at port ${portNumber}');
+console.log(`Server running on port number ${portNumber}`);
+
+app.post('/login', (request, response) => {
+    var username = request.body.username;
+    var password = request.body.password;
+
+    if (username && password) {
+        connection.query('SELECT * FROM user WHERE username = ? and password = ?',
+            [username, password],
+            function(err, result, fields) {
+                if (err) throw err;
+
+                if (result.length > 0) {
+                    request.session.username = username;
+                    request.session.shcart = [];
+                    response.redirect('/');
+                    console.log('Logged in successfully.');
+                } else {
+                    console.log("Login failed.");
+                    response.redirect('/');
+                }
+            }
+        );
+    }
+});
+
+app.get('/', (request, response) => {
+    var username =request.session.usernamel
+    response.render('login',{username:username})
+});
