0502d8ccb15f27c8ffad150891d5e5fb54a7a5d4
JOSE MIGUEL DE JESUS <2221095@slu.edu.ph>
2023-12-10 19:15:30 +0800

    validate functionality for adding users in php and html

--- admin/php/adduser.php
+++ admin/php/adduser.php
@@ -1,12 +1,11 @@
 <?php
-include("admin/php/db.php");
+include("db.php");
 
 if (isset($_POST['add-button'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $firstname = $_POST['first-name'];
     $lastname = $_POST['last-name'];
-    $role = $_POST['role'];
 
     // check if username is already existing 
     $stmt = $conn->prepare("SELECT * FROM user WHERE username=?");
@@ -18,8 +17,8 @@ if (isset($_POST['add-button'])) {
         echo "Username already exists.";
     } else {
         // insert new user
-        $stmt = $conn->prepare("INSERT INTO user (username, password, firstname, lastname, role) VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssss", $username, $password, $firstname, $lastname, $role);
+        $stmt = $conn->prepare("INSERT INTO user (userID, username, password, firstname, lastname) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss", $username, $password, $firstname, $lastname);
 
         if ($stmt->execute()) {
             echo "New record created successfully";
